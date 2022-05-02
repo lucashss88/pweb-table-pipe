db9f10b0cc7be3198ae5e4f6aef1ded1c140ca5d
lucashss88 <lucashss88@gmail.com>
2022-05-01 21:41:16 -0300

    pipe-telefone

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LetraMaiusculaPipe} from './letra-maiuscula.pipe';
 import { CpfPipe } from './cpf.pipe';
+import {TelefonePipe} from './telefone.pipe';
 
 
 
 @NgModule({
-  declarations: [LetraMaiusculaPipe, CpfPipe],
+  declarations: [LetraMaiusculaPipe, CpfPipe, TelefonePipe],
   imports: [
     CommonModule
   ],
   exports: [
     LetraMaiusculaPipe,
-    CpfPipe
+    CpfPipe,
+    TelefonePipe
   ]
 })
 export class PipesModule { }
---
+++ src/app/shared/pipes/telefone.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'telefonePipe'
+})
+export class TelefonePipe implements PipeTransform {
+
+  transform(value: string): string {
+    if (value && value.length === 8) {
+      return `${value.substr(0, 4)}-${value.substr(4, 4)}`;
+    }
+    if (value && value.length === 10) {
+      return `(${value.substr(0, 2)})${value.substr(2, 4)}-${value.substr(6, 4)}`;
+    }
+    if (value && value.length === 11) {
+      return `(${value.substr(0, 3)})${value.substr(3, 4)}-${value.substr(7, 4)}`;
+    }
+    return value;
+  }
+
+}
